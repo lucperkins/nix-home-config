dc3ddbf128944e6810671a7636d6759121e610b9
Luc Perkins <lucperkins@gmail.com>
2020-07-07 16:27:19 -0700

    Fixes to shell config

--- shell.nix
+++ shell.nix
@@ -28,19 +28,26 @@ in {
     history.extended = true;
 
     sessionVariables = {
-      PATH = "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin:$PATH";
+      PATH = "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin:$HOME/.nix-profile/bin:$PATH";
       EDITOR = "nvim";
       GOPATH = "$HOME/go";
     };
 
     initExtra = ''
-      source ~/.zshrc
+      if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+      . ~/.nix-profile/etc/profile.d/nix.sh;
+      export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
+      fi # added by Nix installer
     '';
 
     oh-my-zsh = {
       enable = true;
       plugins = [
+        "docker"
+        "docker-compose"
+        "dotenv"
         "git"
+        "sudo"
       ];
       theme = "muse";
     };
